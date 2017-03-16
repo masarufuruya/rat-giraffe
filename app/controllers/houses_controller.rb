a19fea9331a1daef7a65ffa3219a38f366701f6a
古家大 <yannwennri2@gmail.com>
2017-03-16 11:20:42 +0900

    chage destroy message

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -56,7 +56,7 @@ class HousesController < ApplicationController
   def destroy
     @house.destroy
     respond_to do |format|
-      format.html { redirect_to houses_url, notice: 'House was successfully destroyed.' }
+      format.html { redirect_to houses_url, notice: '物件を削除しました' }
       format.json { head :no_content }
     end
   end
--- app/models/station.rb
+++
@@ -1,4 +0,0 @@
-class Station < ApplicationRecord
-  has_many :nearest_stations
-  has_many :houses, through: :nearest_stations
-end
